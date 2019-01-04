f910d508cfba0d80040e36d223d70ef72eae2c2f
Bhatt, Gaurang <Gaurang.Bhatt@mastercard.com>
2019-01-04 21:07:59 +0530

    intial commit

---
+++ TestJenkin/src/main/java/HelloJenkins.java
@@ -0,0 +1,9 @@
+
+public class HelloJenkins {
+	public static void main(String[] args) {
+		System.out.println("####################################################");
+		System.out.println("................My Jenkins..........................");
+		System.out.println("####################################################");
+	}
+
+}
